6c653bfa76ac02634c36ccbc87fbe2c5187ef937
ShaRuichen <49673290+ShaRuichen@users.noreply.github.com>
2019-12-25 16:42:58 +0800

    Revert "Revert "代码清理并更新版本为1.0.0.1""
    This reverts commit 836d1e65689c968dfe2893caeb61d38ebbf8ff26.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Security.Principal;
-using System.Diagnostics;
-
-using Microsoft.Win32;
 using System.ComponentModel;
-using System.IO;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace EyeCarer
 {
